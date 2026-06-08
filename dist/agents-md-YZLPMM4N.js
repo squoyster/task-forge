c8cbac7ddb460c8b041afcc6538daa117e522040
Aleksander Lesniak <alex@mmmm.local>
2026-06-08 11:31:49 -0600

    checkpoint dist files
    Task: TASK-268
    TaskForge-Managed: true

---
+++ dist/agents-md-YZLPMM4N.js
@@ -0,0 +1,10 @@
+import {
+  installAgentsMd
+} from "./chunk-PKL2HZ5V.js";
+import "./chunk-46G2ACH2.js";
+import "./chunk-5JWCMI7A.js";
+import "./chunk-OPCWHN3N.js";
+export {
+  installAgentsMd
+};
+//# sourceMappingURL=agents-md-YZLPMM4N.js.map
